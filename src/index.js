267e58463c005f6652e2b36ae2a7499fe3b78e96
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-10-09 17:36:53 -0500

    [ADD] add image button and dom manipulation code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-console.log('Happy hacking :)')
+const maximum = 100
+const minimum = 1
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
+
+const createImageNode = () => {
+    const container = document.createElement("div");
+    container.className = "p-4";
+    const imageNode = document.createElement('img');
+    imageNode.className = "mx-auto";
+    imageNode.width = "320";
+    imageNode.src = `https://source.unsplash.com/collection/${random()}`;
+
+    container.appendChild(imageNode);
+
+    return container;
+};
+
+const newImage = createImageNode();
+const mountNode = document.getElementById('imagenes')
+const addButton = document.querySelector('button');
+const addImage = () => {
+  const newImage = createImageNode();
+  mountNode.append(newImage);
+};
+
+
+addButton.addEventListener('click', addImage);
